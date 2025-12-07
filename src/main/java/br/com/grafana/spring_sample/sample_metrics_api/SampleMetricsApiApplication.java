d40f2bc7a8673a12808aed1cfd9fe2cd7563afee
Henrique Cidrin <henriquecidrin96@gmail.com>
2025-12-07 15:12:33 -0300

    initial commit demo project metrics generic api

---
+++ src/main/java/br/com/grafana/spring_sample/sample_metrics_api/SampleMetricsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.grafana.spring_sample.sample_metrics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMetricsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grafana/spring_sample/sample_metrics_api/SampleMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grafana.spring_sample.sample_metrics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
